ca5c34cbc4eb37a6e3101a30026d211402514fb0
cyber3013 <blackking3013@gmail.com>
2023-09-30 23:53:30 +0530

    removed and added at line no 1

--- Trail.java
+++ Trail.java
@@ -6,21 +6,6 @@ import java.util.*;
 
 public class Trail{
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter string literal case : ");
-        String strLiteral = sc.nextLine();
-        System.out.println("Enter string Buffered case : "); 
-        String str = br.readLine();
-        System.out.println(str);
-        System.out.println("The string Buffered case : ");
-        str = "baluchaged";
-        System.out.println(str);
-        strLiteral = "balu cannot be changed";
-        System.out.println(strLiteral);
-
-        FileReader fr = new FileReader("./index.txt");
-        System.out.println("new text added");
-        
+       System.out.println("new line");
     }
 }
